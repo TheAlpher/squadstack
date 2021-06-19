df6d392de55bbb04d2558b8adf9d74b848eaa90c
TheAlpher <TheAlpher>
2021-06-19 20:06:09 +0530

    Update constant validTabKeys

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,5 @@
-const validTabKeys = [0, 1, 2, 3, 4];
-
+import data from "assets/json/data.json";
+const validTabKeys = data.map(obj=>  obj.key);
 const extraPlan = {
   planName: "Enterprise",
   description: "Want more than 80 qualified leads per month",
